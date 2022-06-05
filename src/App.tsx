72920f366f09747cd189a784267d87b91872cef9
Adrian Yeong <yeongwengwah@gmail.com>
2022-06-05 23:58:41 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo">
+      <h1>Todo</h1>
+      <p>Todo application lets you write your daily tasks more efficiently.</p>
+    </div>
+  );
+}
+
+export default App;
